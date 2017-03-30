d8b6f96022bed137860eadf8ad832d1bf4f61760
terence chow <terence.chow.1986@gmail.com>
2017-03-31 00:05:20 +0100

    minify files with clean css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('css', function () {
     return gulp.src('./src/*.scss')
         .pipe(sass())
         .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS())
         .pipe(gulp.dest('./lib'));
 });
 
